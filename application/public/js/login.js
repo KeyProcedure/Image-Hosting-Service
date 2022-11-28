7221c105247d57f515c4257eef6275609cdf47c1
zuby <zubytm@gmail.com>
2022-11-28 10:28:33 -0800

    Updated login page warning messages.

--- application/public/js/login.js
+++ application/public/js/login.js
@@ -1,6 +1,16 @@
 const submitButton = document.getElementById('loginSubmit');
 submitButton.addEventListener('click', validateLogin);
 
+const passwordInputField = document.getElementById('login-username');
+passwordInputField.addEventListener('keydown', function () {
+  passwordInputField.setCustomValidity('')
+});
+
+const confirmPasswordInputField = document.getElementById('login-password');
+confirmPasswordInputField.addEventListener('keydown', function () {
+  confirmPasswordInputField.setCustomValidity('')
+});
+
 function validateLogin() {
   validateUsername();
   validatePassword();
@@ -12,6 +22,8 @@ function validateUsername() {
   if (username.length < 1) {
     document.getElementById('login-username-msg').style.color = 'red';
     document.getElementById('login-username').style.borderColor = 'red';
+    let errorMsg = 'Your username cannot be blank'
+    document.getElementById('login-username').setCustomValidity(errorMsg);
   } else {
     document.getElementById('login-username-msg').style.color = 'black';
     document.getElementById('login-username').style.borderColor = 'black';
@@ -24,6 +36,8 @@ function validatePassword() {
   if (password.length < 1) {
     document.getElementById('login-password-msg').style.color = 'red';
     document.getElementById('login-password').style.borderColor = 'red';
+    let errorMsg = 'Your password cannot be blank'
+    document.getElementById('login-password').setCustomValidity(errorMsg);
   } else {
     document.getElementById('login-password-msg').style.color = 'black';
     document.getElementById('login-password').style.borderColor = 'black';
