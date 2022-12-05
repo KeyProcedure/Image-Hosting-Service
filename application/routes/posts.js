398170951f3b0acec091b2e6fa2e84e5f2413a94
zuby <zubytm@gmail.com>
2022-12-05 11:12:10 -0800

    Updated post.js to resize images smaller than 300x300 to the minimum of their height and width

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../conf/database');
 const multer = require("multer");
 const sharp = require("sharp");
+const {isLoggedIn} = require("../middleware/protectors");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -14,28 +15,33 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({storage: storage})
 
-router.post("/create", upload.single("uploadImage"), function(req, res, next) {
+router.post("/create", isLoggedIn, upload.single("uploadImage"), async function (req, res, next) {
   let uploadedFile = req.file.path;
   let thumbnailName = `thumbnail-${req.file.filename}`;
   let destinationOfThumbnail = `${req.file.destination}/${thumbnailName}`;
   const {title, description} = req.body;
   const userId = req.session.userId;
+  const metadata = await sharp(uploadedFile).metadata();
 
+  if (metadata.width < 300 || metadata.height < 300) {
+    sharp(uploadedFile).resize(Math.min(metadata.width), metadata.height);
+  } else {
+    sharp(uploadedFile).resize(300);
+  }
   sharp(uploadedFile)
-    .resize(300)
     .toFile(destinationOfThumbnail)
-    .then(function() {
+    .then(function () {
       let baseSQL = `
       INSERT INTO posts (title, description, image, thumbnail, fk_authorId) VALUE (?,?,?,?,?)
       `
-      return db.query(baseSQL,[title, description, uploadedFile, destinationOfThumbnail, userId])
+      return db.query(baseSQL, [title, description, uploadedFile, destinationOfThumbnail, userId])
     })
-    .then(function([results, fields]) {
+    .then(function ([results, fields]) {
       if (results && results.affectedRows) {
         req.flash("success", "Your post has been created!");
-        req.session.save(function(saveErr) {
+        req.session.save(function (saveErr) {
           res.redirect('/');
         })
       }
@@ -43,8 +49,8 @@ router.post("/create", upload.single("uploadImage"), function(req, res, next) {
     .catch(err => next(err));
 });
 
-// localhose:3000/posts/search
-router.get("/search", function(req, res, next) {
+// localhost:3000/posts/search
+router.get("/search", function (req, res, next) {
   let searchTerm = `%${req.query.searchTerm}%`;
   let originalSearchTerm = req.query.searchTerm;
   let baseSQL =
@@ -52,22 +58,21 @@ router.get("/search", function(req, res, next) {
     FROM posts
     HAVING haystack like ?;`;
   db.execute(baseSQL, [searchTerm])
-    .then(function([results, fields]) {
+    .then(function ([results, fields]) {
       if (originalSearchTerm === "" || originalSearchTerm === " " || results.length < 1) {
         req.flash("error", `0 results found`);
-        req.session.save(function(saveErr) {
+        req.session.save(function (saveErr) {
           res.redirect(`/`);
         })
-      }
-      else {
+      } else {
         res.locals.results = results;
         res.locals.searchTerm = originalSearchTerm;
         req.flash("success", `${results.length} results found`);
-        req.session.save(function(saveErr) {
+        req.session.save(function (saveErr) {
           res.render(`index`);
         })
       }
-  })
+    })
 });
 
 module.exports = router;
