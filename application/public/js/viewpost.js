ff3bde2c75202f88f6298624cdaa30372ee97692
zuby <zubytm@gmail.com>
2022-12-07 15:56:51 -0800

    Added user data to comments

--- application/public/js/viewpost.js
+++ application/public/js/viewpost.js
@@ -1,5 +1,17 @@
 function addNewComment(data) {
+  let commentList = document.getElementById('comment-list');
+  let newComment = document.createElement('template');
+  newComment.innerHTML = `
+    <div id="message-${data.commentId}" class="comment">
+      <strong class="comment-author">${data.username}</strong>
+      <span class="comment-date">${new Date().toLocaleString("en-US", {
+        timeStyle:"short",
+        dateStyle:"short"
+      })}</span>
+      <div class="comment-text">${data.comment}</div>
+     </div>`;
 
+  commentList.append(newComment.content);
 }
 
 document.getElementById('comment-button')
@@ -7,6 +19,10 @@ document.getElementById('comment-button')
     let commentTextElement = document.getElementById('comment-text');
     let commentText = commentTextElement.value;
     let postId = ev.currentTarget.dataset.postid;
+    let commentId = ev.currentTarget.dataset.id;
+
+    if (!commentText)
+      return
 
     fetch("/comments/create", {
       method: "POST",
@@ -20,9 +36,9 @@ document.getElementById('comment-button')
     })
       .then(response => response.json())
       .then(res_json => {
-        console.log(res_json);
+        // addNewComment(res_json.data);
         location.reload();
+        document.getElementById(commentId).scrollIntoView();
       })
       .catch(err => console.log(err));
-
 })
